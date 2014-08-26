3afdef0f0734ec2e6e0f0a66410e96473f18ed76
Luke Petschauer <lpetschauer@gmail.com>
2014-08-26 20:51:51 +0800

    sqle.py - try/except

--- sql/sqle.py
+++ sql/sqle.py
@@ -1,2 +1,23 @@
 # try/except
 
+import sqlite3
+
+
+# create the connection object
+conn = sqlite3.connect("new.db")
+
+# get a cursor object
+cursor = conn.cursor()
+
+try:
+    # insert data
+    cursor.execute("INSERT INTO populations VALUES('New York City', 'NY', 8200000)")
+    cursor.execute("INSERT INTO populations VALUES('San Francisco', 'CA', 800000)")
+
+    # commit changes
+    conn.commit()
+
+except sqlite3.OperationalError:
+    raise NameError
+
+conn.close()
