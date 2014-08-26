6ed23360724a3efe80a27c5d7cfe7ed66dfc2245
Luke Petschauer <lpetschauer@gmail.com>
2014-08-26 20:59:50 +0800

    sqlf.py - Using select to print each row pulls out tuples of strings tagged as unicode!

--- sql/sqlf.py
+++ sql/sqlf.py
@@ -1,2 +1,13 @@
 # SELECT statement
 
+import sqlite3
+
+
+with sqlite3.connect("new.db") as connection:
+    c = connection.cursor()
+
+    # use a for loop to iterate through the db, printing results line by line.
+    for row in c.execute("SELECT firstname, lastname from employees"):
+        print row
+
+    # the above prints ugly tuples of strings tagged with u (for unicode).
