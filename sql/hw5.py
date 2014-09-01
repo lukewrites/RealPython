9a4bae4c787fbe5f9f97c06cd2dc19d3d256f9b6
Luke Petschauer <lpetschauer@gmail.com>
2014-09-01 21:38:10 +0800

    hw4.py done

--- sql/hw5.py
+++ sql/hw5.py
@@ -2,3 +2,10 @@
 Finally, output the car's make and model on one line, the quantity on another
 line, and then the order_dates on subsequent lines below that.
 """
+
+import sqlite3
+
+
+with sqlite3.connect("cars.db") as connection:
+    c = connection.cursor()
+
