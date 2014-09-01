b08160fab2076a9d7f5b692977e2d33b25baf80a
Luke Petschauer <lpetschauer@gmail.com>
2014-09-01 20:47:45 +0800

    last HW assignments for SQL section.

---
+++ sql/hw4.py
@@ -0,0 +1,6 @@
+"""
+Add another table to accompany your 'inventory' table called 'orders'.
+This table should have the following fields: 'make', 'model', and
+'order_date'. Add 15 records (3 for each car), each with a separate order
+date (YYYY-MM-DD). Research how to do that.
+"""
---
+++ sql/hw5.py
@@ -0,0 +1,4 @@
+"""
+Finally, output the car's make and model on one line, the quantity on another
+line, and then the order_dates on subsequent lines below that.
+"""
