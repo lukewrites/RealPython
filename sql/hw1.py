bc5ac11cc9505b5c494226ab38d3b61dc71f702e
Luke Petschauer <lpetschauer@gmail.com>
2014-09-01 14:22:08 +0800

    first homework (page 61)

---
+++ sql/hw1.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+""" Using the “inventory” table from the previous homework assignment,
+add(INSERT) 5records (rows of data) to the table. Make sure 3 of the vehicles are
+Fords while the other 2 are Hondas. Use any model and quantity for each.
+"""
+
