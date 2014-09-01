1fd92fc19dd95581460f767c2a8fe325e691d7a7
Luke Petschauer <lpetschauer@gmail.com>
2014-09-01 14:26:53 +0800

    first homework (page 61) finished. insertmany() into database.

--- sql/hw1.py
+++ sql/hw1.py
@@ -4,3 +4,17 @@ add(INSERT) 5records (rows of data) to the table. Make sure 3 of the vehicles ar
 Fords while the other 2 are Hondas. Use any model and quantity for each.
 """
 
+import sqlite3
+
+with sqlite3.connect("cars.db") as connection:
+    c = connection.cursor()
+
+    new_inventory = [
+        ('Ford', 'Escort', 23),
+        ('Honda', 'Fit', 15),
+        ('Ford', 'Escape', 2),
+        ('Honda', 'CR-V', 8),
+        ('Ford', 'F150', 1)
+    ]
+
+    c.executemany("INSERT INTO inventory VALUES(?, ?, ?)", new_inventory)
