98e31f5e165ea819c0b5b35df350f4b02658dcd8
Luke Petschauer <lpetschauer@gmail.com>
2014-09-01 15:28:31 +0800

    sqll.py
    more with JOIN

--- sql/sqlk.py
+++ sql/sqlk.py
@@ -1,1 +1,16 @@
 # JOINs in SQL.
+
+import sqlite3
+
+
+with sqlite3.connect("new.db") as connection:
+    c = connection.cursor()
+
+    c.execute("""SELECT population.city, population.population, regions.region
+                FROM population, regions
+                WHERE population.city = regions.city""")
+
+    rows = c.fetchall()
+
+    for r in rows:
+        print r[0], r[1], r[2]
