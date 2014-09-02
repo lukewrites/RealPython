e4963a13ee76de89378a54f8bd510ab0678687be
Luke Petschauer <lpetschauer@gmail.com>
2014-09-02 09:24:07 +0800

    hw5.py finished.
    Looks like I used a roundabout way to do this; the suggested answer is a bit shorter. I used sqll.py as my model.

--- sql/hw5.py
+++ sql/hw5.py
@@ -9,6 +9,14 @@ import sqlite3
 with sqlite3.connect("cars.db") as connection:
     c = connection.cursor()
 
-    c.execute()
+    c.execute("""SELECT inventory.Make, inventory.Model,
+                  inventory.Quantity, orders.order_date FROM inventory, orders
+                  WHERE inventory.Model == orders.make
+                  ORDER BY inventory.Make DESC""")
 
     rows = c.fetchall()
+
+    for r in rows:
+        print r[0], r[1]
+        print 'Quantity: ', r[2]
+        print r[3], '\n'
