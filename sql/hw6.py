8f7354b09d4ad905a6fe746fc4076166563bd3c4
Luke Petschauer <lpetschauer@gmail.com>
2014-09-02 10:38:15 +0800

    hw6.py on the right path now
    still seems a bit clunky. I don't like having to hard-wire in the names of the makes.
    think there's a better way to do this in which I use a loop to pull out makes, get the data, and then report it.

--- sql/hw6.py
+++ sql/hw6.py
@@ -1,6 +1,18 @@
+"""
+Using the COUNT() function, calculate the total number of order for each make
+and model.
+"""
+
 import sqlite3
 
 
 with sqlite3.connect('cars.db') as connection:
     c = connection.cursor()
 
+    sql = {
+            "SELECT count(make) from orders WHERE make = 'Escape'",
+            "SELECT count(make) from orders WHERE make = 'Escort'",
+            "SELECT count(make) from orders WHERE make = 'F150'",
+            "SELECT count(make) from orders WHERE make = 'CR-V'",
+            "SELECT count(make) from orders WHERE make = 'Fit'",
+    }
