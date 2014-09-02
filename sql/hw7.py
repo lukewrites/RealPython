00d0f75810afe366b53df6f867167d6126bebe3a
Luke Petschauer <lpetschauer@gmail.com>
2014-09-02 16:21:02 +0800

    last hw - not working

---
+++ sql/hw7.py
@@ -0,0 +1,20 @@
+"""
+Output car's make & model on one line, quantity on the next, and then the order
+count on the next line.
+"""
+
+import sqlite3
+
+
+with sqlite3.connect("cars.db") as connect:
+    c = connect.cursor()
+    # get make, model, and quantity
+    c.execute("""SELECT * from inventory""")
+    rows = c.fetchall()
+    # print make & model on one line
+    for row in rows:
+        print row[0], row[1], '\n', row[2]
+        # print quantity
+        c.execute("SELECT count(order_date) FROM orders WHERE make=?", (row[0]))
+        order_count = c.fetchone()[0]
+        print order_count
