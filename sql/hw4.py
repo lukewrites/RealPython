e0062602f4a44436c3706393a8696c4218955c47
Luke Petschauer <lpetschauer@gmail.com>
2014-09-02 09:09:55 +0800

    hw4.py revised
    Turns out sqlite3 doesn't have a DATE type. Have to use str. Stupid stupid stupid.

--- sql/hw4.py
+++ sql/hw4.py
@@ -12,25 +12,25 @@ with sqlite3.connect("cars.db") as connection:
     c = connection.cursor()
 
     c.execute("""CREATE TABLE orders
-              (make TEXT, model TEXT, order_date DATE)
+              (make TEXT, model TEXT, order_date TEXT)
               """)
 
     sales_data = [
-        ('Fit', 'Sport', 2014-03-03),
-        ('Fit', 'Base', 2013-05-05),
-        ('Escape', 'Luxe', 2014-06-06),
-        ('Escort', 'Base', 2013-01-01),
-        ('CR-V', 'Base', 2014-05-05),
-        ('CR-V', 'Base', 2014-05-06),
-        ('CR-V', 'Sport', 2014-03-03),
-        ('Escape', 'Luxe', 2014-06-06),
-        ('Escort', 'Base', 2014-11-11),
-        ('F150', 'Base', 2014-02-02),
-        ('Escape', 'Sport', 2014-12-12),
-        ('F150', 'Diesel', 2014-02-05),
-        ('F150', 'Diesel', 2014-07-12),
-        ('F150', 'Diesel', 2014-06-12),
-        ('Escort', 'Base', 2014-11-10)
+        ('Fit', 'Sport', '2014-03-03'),
+        ('Fit', 'Base', '2013-05-05'),
+        ('Escape', 'Luxe', '2014-06-06'),
+        ('Escort', 'Base', '2013-01-01'),
+        ('CR-V', 'Base', '2014-05-05'),
+        ('CR-V', 'Base', '2014-05-06'),
+        ('CR-V', 'Sport', '2014-03-03'),
+        ('Escape', 'Luxe', '2014-06-06'),
+        ('Escort', 'Base', '2014-11-11'),
+        ('F150', 'Base', '2014-02-02'),
+        ('Escape', 'Sport', '2014-12-12'),
+        ('F150', 'Diesel', '2014-02-05'),
+        ('F150', 'Diesel', '2014-07-12'),
+        ('F150', 'Diesel', '2014-06-12'),
+        ('Escort', 'Base', '2014-11-10')
     ]
 
     c.executemany("INSERT INTO orders VALUES(?, ?, ?)", sales_data)
--- sql/hw5.py
+++ sql/hw5.py
@@ -9,3 +9,6 @@ import sqlite3
 with sqlite3.connect("cars.db") as connection:
     c = connection.cursor()
 
+    c.execute()
+
+    rows = c.fetchall()
