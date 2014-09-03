8b1ab20f1b10748737ab470a3490d0f4309741ba
Luke Petschauer <lpetschauer@gmail.com>
2014-09-03 14:42:48 +0800

    continuing on flask blog project

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -4,15 +4,36 @@ import sqlite3
 
 # configuration
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'hard_to_guess'
 
 app = Flask(__name__)
 
 # pulls in app configuration by looking for UPPERCASE variables
 app.config.from_object(__name__)
 
+
 # function used for connection to the database
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or \
+                        request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+    return render_template('login.html')
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
