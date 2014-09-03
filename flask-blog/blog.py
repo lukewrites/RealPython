7235e15b70177fa16327e321abb9998f4153fb3c
Luke Petschauer <lpetschauer@gmail.com>
2014-09-03 17:22:09 +0800

    up to page 84

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -23,17 +23,24 @@ def connect_db():
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME'] or \
-                        request.form['password'] != app.config['PASSWORD']:
+        if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
             error = 'Invalid Credentials. Please try again.'
         else:
             session['logged_in'] = True
             return redirect(url_for('main'))
-    return render_template('login.html')
+    return render_template('login.html', error=error)
+
 
 @app.route('/main')
 def main():
     return render_template('main.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
