f6fcab623d612b875c9e759a4c84e94e9be11139
Luke Petschauer <lpetschauer@gmail.com>
2014-09-03 11:39:47 +0800

    set up flask-blog project

--- sql/assignment3b.py
+++ sql/assignment3b.py
@@ -1,5 +1,19 @@
 import sqlite3
 
-# user prompt: aggregation or exit
 
-# aggregations: AVG, MIN, MAX, SUM
+# user prompt: aggregation or exit
+def welcome():
+    task = raw_input("""Which of the following would you like to do?
+                        1. Calculate average
+                        2. Find minimum value
+                        3. Find maximum value
+                        4. Calculate the sum
+                        5. Quit""")
+    # basic setup: connect to the db
+    with sqlite3.connect("newnum.db") as connection:
+        c = connection.cursor()
+        while True:
+            if task is not in range(5):
+                welcome()
+
+    # aggregations: AVG, MIN, MAX, SUM
