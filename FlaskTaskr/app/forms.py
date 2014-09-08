51b52d9bfaf8774f4faa6251dc63c0c2d72876d8
Luke Petschauer <lpetschauer@gmail.com>
2014-09-08 16:56:40 +0800

    Working. Was missing a pair of parenthesis. Pwnt.

--- FlaskTaskr/app/forms.py
+++ FlaskTaskr/app/forms.py
@@ -7,7 +7,7 @@ class RegisterForm(Form):
     name = TextField('Username', validators=[DataRequired(), Length(min=6, max=25)])
     email = TextField('Email', validators=[DataRequired(), Length(min=6, max=25)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=40)])
-    confirm = PasswordField('Repeat password', validators=[DataRequired, EqualTo('password', message='Passwords must match')])
+    confirm = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
 
 
 class LoginForm(Form):
