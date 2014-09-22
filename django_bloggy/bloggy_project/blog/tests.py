4c71f9b1b3b60e34e93445b7b7bc3673ddaf670c
Luke Petschauer <luke@lukewrites.com>
2014-09-22 10:20:52 +0800

    working tests

--- django_bloggy/bloggy_project/blog/tests.py
+++ django_bloggy/bloggy_project/blog/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from blog.models import Post  # this is different to the book.
 
-# Create your tests here.
+
+class PostTest(TestCase):
+
+    def test_str(self):
+        my_title = Post(title='This is a basic title')
+        self.assertEquals(str(my_title), 'This is a basic title')
+        #  interesting that we still need the str(my_title) w/py3.
